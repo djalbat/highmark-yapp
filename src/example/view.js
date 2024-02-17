dcae2c38585025e4e6504e03ad14b28c07f015a9
James Smith <james.smith@djalbat.com>
2024-02-17 17:27:08 +0000

    Small fixes.

--- src/example/view.js
+++ src/example/view.js
@@ -66,24 +66,11 @@ class View extends Element {
   }
 
   static initialContent = `
-In order to get started you can do one of the following:
+export default withStyle(simpleApplication)\`
 
-Have a look at this site in your browser's developer tools.
-Clone the Yapp pretty printer's repository and run the example.
-Clone the Juxtapose repository and build the examples.
-Firstly, if you choose to have a look at this site in your browser's developer tools then a good place to start is the src/main.js file. The other file that should be of interest is its sibling view.js file. After that, maybe just look around.
+  color: #333;
 
-This site should be taken as an example, by the way, not as a blueprint. Experience has taught that certain approaches work well, but nothing is written in stone.
-
-Also bear in mind that JSX is not ideal for embedding large amounts of content into a site or application. The articles here demonstrate what is about the practical limit.
-
-Secondly, here is the link to the Yapp pretty printer's repository:
-
-https://github.com/djalbat/yapp
-
-This is a more comprehensive project and shows what is possible with Juxtapose. The code listings on this site use Yapp, for example. The project's example application also makes use of several Easy projects.
-
-Lastly, the octocat logo, top right, is a link to the Juxtapose repository on GitHub. This provides the means to set up a working environment for Juxtapose. The remaining articles here tacitly assume that you have done so, but it is not essential.
+\`;
 `;
 
   static tagName = "div";
