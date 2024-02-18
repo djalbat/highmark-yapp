82607d6802e7a6b2c852ef6ab8f2935518eb1791
James Smith <james.smith@djalbat.com>
2024-02-18 11:04:20 +0000

    Propagated.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 "use strict";
 
-export { default as Listing } from "./listing";
 export { default as MarkdownLexer } from "./markdown/lexer";
 export { default as MarkdownParser } from "./markdown/parser";
